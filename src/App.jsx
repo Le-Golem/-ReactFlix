17b33a5c1762ea8c3754e952b00df275b3c101ba
Gauthier Chapon <114396437+gautchap@users.noreply.github.com>
2022-12-21 17:33:57 +0100

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import Header from "./components/Header";
 import Profil from "./components/Profil";
 
 const App = () => {
     return (
         <>
-            <Header>
-                <Profil />
-            </Header>
+            <Profil />
         </>
     );
 };
